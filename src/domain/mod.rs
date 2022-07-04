18774a7eb9e1e139b302d26f8411279726770baa
afsec <97892233+afsec@users.noreply.github.com>
2022-07-04 13:52:55 -0300

    refactor: fix clean architecture concepts

--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -1,3 +1,7 @@
+/// Core business logic but is independent of the application.
+/// The domain layer is the innermost layer and according to the dependency
+/// rule it should be independent of everything
+
 pub(crate) mod user;
 
 
@@ -6,3 +10,16 @@ pub(crate) mod user;
 /// persisted using JPA @Entity are usually DDD entities.
 pub(crate) trait Entity {}
 
+/// Service – an object that implements business logic which doesn’t belong in an entity or
+///  a value object.
+pub(crate) trait Service {}
+
+/// Factory – an object or method that implements object creation logic which is too
+/// complex to be done directly by a constructor. A factory might be implemented as a
+///  static method of a class.
+pub(crate) trait Factory {}
+
+/// Value object – an object which is a collection of values. Two value objects whose
+/// attributes have the same values can be used interchangeably. An example of a value
+/// object is a Money class, which consists of a currency and an amount.
+pub(crate) trait Value {}
